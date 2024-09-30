2fa115d096e13dd8f981c76a79f244ce4fed2f4e
nnikitenko <nnikitenko@aorti.ru>
2024-09-30 12:35:45 +0300

    Первоначальная настройка проекта. Добавлено приложение "Сотрудники", настроена авторизация. Настроен вывод документации.

---
+++ employees/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from employees.apps import EmployeesConfig
+from employees.views import EmployeeCreateApiView, EmployeeListApiView, EmployeeRetrieveApiView, EmployeeUpdateApiView, \
+    EmployeeDestroyApiView
+
+app_name = EmployeesConfig.name
+
+urlpatterns = [
+    path("register/", EmployeeCreateApiView.as_view(), name="register"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=[AllowAny]),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=[AllowAny]),
+        name="token_refresh",
+    ),
+    path("", EmployeeListApiView.as_view(), name="employees_list"),
+    path("<int:pk>/", EmployeeRetrieveApiView.as_view(), name="employee_retrieve"),
+    path("<int:pk>/update/", EmployeeUpdateApiView.as_view(), name="employee_update"),
+    path("<int:pk>/delete/", EmployeeDestroyApiView.as_view(), name="employee_destroy"),
+]
---
+++ employees/views.py
@@ -0,0 +1,46 @@
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, ListAPIView
+from rest_framework.permissions import AllowAny
+
+from employees.models import Employee
+from employees.serializers import EmployeeSerializer
+
+
+class EmployeeCreateApiView(CreateAPIView):
+    """создание сотрудника"""
+
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        employee = serializer.save(is_active=True)
+        employee.set_password(employee.password)
+        employee.save()
+
+
+class EmployeeRetrieveApiView(RetrieveAPIView):
+    """получение данных сотрудника по id"""
+
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+
+class EmployeeListApiView(ListAPIView):
+    """получение списка сотрудников"""
+
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+
+class EmployeeUpdateApiView(UpdateAPIView):
+    """обновление данных сотрудника"""
+
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+
+class EmployeeDestroyApiView(DestroyAPIView):
+    """удаление данных сотрудника"""
+
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
